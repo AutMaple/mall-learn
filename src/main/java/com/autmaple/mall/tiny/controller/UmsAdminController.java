a771b1edf0866ffde0260a80daf9312f53e41338
AutMaple <autmaple609@qq.com>
2022-06-15 20:46:03 +0800

    添加创建数据库和表的 sql 文件

---
+++ src/main/java/com/autmaple/mall/tiny/controller/UmsAdminController.java
@@ -0,0 +1,9 @@
+package com.autmaple.mall.tiny.controller;/**
+  * ClassName UmsAdminController
+  * Description TODO
+  * @Author AutMaple
+  * @Date 2022/6/15 20:25
+  * Version 1.0
+  **/
+public class UmsAdminController {
+}
