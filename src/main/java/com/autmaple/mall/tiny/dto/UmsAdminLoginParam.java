91f1f1dfb7f2378044add80c935f3b260c55126c
AutMaple <autmaple609@qq.com>
2022-06-15 21:27:01 +0800

    添加管理员登陆参数对应的实体对象

---
+++ src/main/java/com/autmaple/mall/tiny/dto/UmsAdminLoginParam.java
@@ -0,0 +1,39 @@
+package com.autmaple.mall.tiny.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * ClassName UmsAdminLoginParam
+ * Description 管理员登录参数
+ *
+ * @Author AutMaple
+ * @Date 2022/6/15 21:23
+ * Version 1.0
+ **/
+public class UmsAdminLoginParam {
+    @ApiModelProperty(value = "用户名", required = true)
+    @NotEmpty(message = "用户名不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "密码", required = true)
+    @NotEmpty(message = "密码不能为空")
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
