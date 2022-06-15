ddc79891ae3cceec52f36987be5bb5c055d1c767
AutMaple <autmaple609@qq.com>
2022-06-15 22:52:16 +0800

    增添一个 dao 包的扫描路径

--- src/main/java/com/autmaple/mall/tiny/config/MybatisConfig.java
+++ src/main/java/com/autmaple/mall/tiny/config/MybatisConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.autmaple.mall.tiny.mbg.mapper")
+@MapperScan({"com.autmaple.mall.tiny.mbg.mapper", "com.autmaple.mall.tiny.dao"})
 public class MybatisConfig {
 
 }
