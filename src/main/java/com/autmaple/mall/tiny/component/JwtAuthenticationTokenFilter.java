bc702db904658481a85ac231f424016a70f79b3e
AutMaple <autmaple609@qq.com>
2022-06-16 21:08:07 +0800

    添加 JWT 登录认证过滤器

---
+++ src/main/java/com/autmaple/mall/tiny/component/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,75 @@
+package com.autmaple.mall.tiny.component;
+
+import cn.hutool.core.lang.Chain;
+import com.alibaba.druid.filter.FilterChainImpl;
+import com.autmaple.mall.tiny.common.utils.JwtTokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.security.sasl.AuthorizeCallback;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * ClassName JwtAuthenticationTokenFilter
+ * Description JWT 登录认证过滤器
+ *
+ * @Author AutMaple
+ * @Date 2022/6/16 20:31
+ * Version 1.0
+ **/
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationTokenFilter.class);
+
+    private final UserDetailsService userDetailsService;
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Value("${jwt.tokenHeader}")
+    private String tokenHeader;
+
+    @Value("@{jwt.tokenHead")
+    private String tokenHead;
+
+    @Autowired
+    public JwtAuthenticationTokenFilter(UserDetailsService userDetailsService, JwtTokenUtil jwtTokenUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(this.tokenHeader);
+        if(authHeader != null && authHeader.startsWith(this.tokenHead)){
+            String authToken = authHeader.substring(this.tokenHead.length()); // 在 Bearer 后面的部分
+            String username = jwtTokenUtil.getUsernameFromToken(authToken);
+            LOGGER.info("Checking username: {}", username);
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                if(jwtTokenUtil.validateToken(authToken, userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    LOGGER.info("Authenticated user: {}", username);
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
