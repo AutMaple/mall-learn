7fb5c88c5d456cf50f587766adecb78c68b10d28
AutMaple <autmaple609@qq.com>
2022-06-16 19:57:45 +0800

    添加未授权时默认返回的数据

---
+++ src/main/java/com/autmaple/mall/tiny/component/RestfulAccessDeniedHandler.java
@@ -0,0 +1,9 @@
+package com.autmaple.mall.tiny.component;/**
+  * ClassName RestfulAccessDeniedHandler
+  * Description TODO
+  * @Author AutMaple
+  * @Date 2022/6/16 19:51
+  * Version 1.0
+  **/
+public class RestfulAccessDeniedHandler {
+}
