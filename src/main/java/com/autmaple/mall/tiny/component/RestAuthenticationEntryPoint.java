dac5173361d63ea17990134a94e5d02322fc5a34
AutMaple <autmaple609@qq.com>
2022-06-16 20:06:34 +0800

    添加未登录时，返回自定义结果的处理类

---
+++ src/main/java/com/autmaple/mall/tiny/component/RestAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.autmaple.mall.tiny.component;
+
+import cn.hutool.json.JSONUtil;
+import com.autmaple.mall.tiny.common.api.CommonResult;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * ClassName RestAuthenticationEntryPoint
+ * Description 未登录时或者 token 失效时，返回自定义的结果
+ *
+ * @Author AutMaple
+ * @Date 2022/6/16 20:02
+ * Version 1.0
+ **/
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(e.getMessage())));
+        response.getWriter().flush();
+    }
+}
