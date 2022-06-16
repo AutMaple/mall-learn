3698e46100175fd4c74b465da667b8c8a02935a1
AutMaple <autmaple609@qq.com>
2022-06-16 21:32:49 +0800

    添加 SpringSecurity 配置类

--- src/main/java/com/autmaple/mall/tiny/config/SecurityConfig.java
+++ src/main/java/com/autmaple/mall/tiny/config/SecurityConfig.java
@@ -1,8 +1,33 @@
 package com.autmaple.mall.tiny.config;
 
+import com.autmaple.mall.tiny.component.JwtAuthenticationTokenFilter;
+import com.autmaple.mall.tiny.component.RestAuthenticationEntryPoint;
+import com.autmaple.mall.tiny.component.RestfulAccessDeniedHandler;
+import com.autmaple.mall.tiny.dto.AdminUserDetails;
+import com.autmaple.mall.tiny.mbg.model.UmsAdmin;
+import com.autmaple.mall.tiny.mbg.model.UmsPermission;
+import com.autmaple.mall.tiny.service.UmsAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.UserDetailsAwareConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 
 /**
@@ -11,6 +36,86 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
-    
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UmsAdminService adminService;
+    private final RestfulAccessDeniedHandler restfulAccessDeniedHandler;
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    private final JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    @Autowired
+    public SecurityConfig(UmsAdminService adminService,
+                          RestfulAccessDeniedHandler restfulAccessDeniedHandler,
+                          RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+                          JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter) {
+        this.adminService = adminService;
+        this.restfulAccessDeniedHandler = restfulAccessDeniedHandler;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.jwtAuthenticationTokenFilter = jwtAuthenticationTokenFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf() // 由于使用的是 JWT， 我们不需要 CSRF
+                .disable()
+                .sessionManagement() // 基于 token, 所以不需要 session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/swagger-resources/**",
+                        "/v2/api-docs/**"
+                )
+                .permitAll()
+                //.antMatchers("/**") // 测试时，所有资源均允许访问
+                //.permitAll()
+                .anyRequest() // 除了上面的请求外，其他的请求都需要进行鉴权认证
+                .authenticated();
+
+        // 禁用缓存
+        httpSecurity.headers().cacheControl();
+
+        // 添加 JWT 过滤器
+        httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // 添加自定义未授权和未登录请求的返回结果
+        httpSecurity.exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                .authenticationEntryPoint(restAuthenticationEntryPoint);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            UmsAdmin admin = adminService.getAdminByUsername(username);
+            if(admin != null){
+                List<UmsPermission> permissionList = adminService.getPermissionList(admin.getId());
+                return new AdminUserDetails(admin, permissionList);
+            }
+            throw new UsernameNotFoundException("用户名或者密码错误");
+
+        };
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 }
