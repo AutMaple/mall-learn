ca1889b239265a8aaaae6be07326a6953d1f67d9
AutMaple <autmaple609@qq.com>
2022-06-19 17:58:08 +0800

    添加 SpriingTask 的配置，笔记以及对应的定时任务

---
+++ src/main/java/com/autmaple/mall/tiny/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,29 @@
+package com.autmaple.mall.tiny.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @ClassName OrderTimeOutCancelTask
+ * @Description 订单超时取消并解锁库存的定时器
+ * @Author AutMaple
+ * @Date 2022/6/19 17:40
+ * @Version 1.0
+ **/
+@Component
+public class OrderTimeOutCancelTask {
+    private Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    /**
+      * @Author AutMaple
+      * @Description 每 10 分钟扫描一次，扫描设定超时时间之前下的订单，如果没有支付就取消订单
+      * @Date 2022/6/19 17:45
+      **/
+    @Scheduled(cron = "0 0/10 * ? * ?")
+    private void cancelTimeOutOrder() {
+        LOGGER.info("取消订单，并根据 sku 编号释放锁定的库存");
+    }
+}
---
+++ src/main/java/com/autmaple/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,16 @@
+package com.autmaple.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @ClassName SpringTaskConfig
+ * @Description SpringTask 配置类
+ * @Author AutMaple
+ * @Date 2022/6/19 17:50
+ * @Version 1.0
+ **/
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
